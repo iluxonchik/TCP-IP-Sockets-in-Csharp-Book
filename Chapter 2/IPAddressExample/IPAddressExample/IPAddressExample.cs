d0a2ed2a14a48ae34dbddcaf085d5a630c5bf01e
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-06-22 14:14:05 +0100

    Added IPHostExample from Chapter 2.

---
+++ Chapter 2/IPAddressExample/IPAddressExample/IPAddressExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPAddressExample
+{
+    public static class IPAddressExample
+    {
+        public static void PrintHostInfo(String host)
+        {
+            try
+            {
+                IPHostEntry hostInfo;
+
+                // Attemp to resolve DNS for a given host or address
+                hostInfo = Dns.GetHostEntry(host);
+
+                // Display the primary host name
+                Console.WriteLine("\tCanonical Name: " + hostInfo.HostName);
+
+                // Display the list of IP addresses for this host
+                Console.Write("\tIP Addresses: ");
+                foreach(IPAddress ipaddr in hostInfo.AddressList)
+                {
+                    Console.Write(ipaddr.ToString() + " ");
+                }
+                Console.WriteLine();
+
+                // Display list of alias names for this host
+                Console.Write("\tAlisases: ");
+
+                foreach(String alias in hostInfo.Aliases)
+                {
+                    Console.Write(alias +  " ");
+                }
+                Console.WriteLine();
+            } catch (Exception)
+            {
+                Console.WriteLine("\t Unable to resolve host: " + host);
+            }
+        }
+    }
+}
---
+++ Chapter 2/IPAddressExample/IPAddressExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPAddressExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get and print local host info
+            try
+            {
+                Console.WriteLine("Local Host:");
+                string localHostName = Dns.GetHostName();
+                Console.WriteLine("\tHost Name: " + localHostName);
+
+                IPAddressExample.PrintHostInfo(localHostName);
+            } catch (Exception)
+            {
+                Console.WriteLine("Unable to resolve local host");
+            }
+
+            // Get and print hosts given on command line
+            foreach(String arg in args)
+            {
+                Console.WriteLine(arg + ":");
+                IPAddressExample.PrintHostInfo(arg);
+            }
+        }
+    }
+}
