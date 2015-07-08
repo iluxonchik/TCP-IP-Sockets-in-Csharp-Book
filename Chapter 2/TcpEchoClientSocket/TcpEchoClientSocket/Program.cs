e66c70fe8f69ed2930d61f230015acc0f1178639
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-07-08 13:46:32 +0100

    TcpEchoServerSocket and TcpEchoClientSocket projects added.

---
+++ Chapter 2/TcpEchoClientSocket/TcpEchoClientSocket/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpEchoClientSocket
+{
+    class TcoEchoClientSocket { 
+        static void Main(string[] args)
+        {
+            if ((args.Length < 2) || (args.Length > 3))
+            {
+                throw new ArgumentException("Parameters: <Server> <Word> [<Port>]");
+            }
+
+            string server = args[0];
+
+            byte[] byteBuffer = Encoding.ASCII.GetBytes(args[1]);
+
+            int servPort = (args.Length == 3) ? int.Parse(args[2]) : 7;
+
+            Socket sock = null;
+
+            try
+            {
+                // Create a TCP socket instance
+                sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint serverEndPoint = new IPEndPoint(Dns.GetHostEntry(server).AddressList[1], servPort);
+
+                // Connect the socket to server on specified port
+                sock.Connect(serverEndPoint);
+                Console.WriteLine("Connecting to server... Sending echo string");
+
+                // Send the encoded string to the server
+                sock.Send(byteBuffer, byteBuffer.Length, SocketFlags.None);
+
+                Console.WriteLine("Send {0} bytes to server", byteBuffer.Length);
+
+                int totalBytesRcvd = 0;
+                int bytesRcvd = 0;
+                
+                while (totalBytesRcvd < byteBuffer.Length)
+                {
+                    if ((bytesRcvd = sock.Receive(byteBuffer, totalBytesRcvd, byteBuffer.Length - totalBytesRcvd, SocketFlags.None)) == 0)
+                    {
+                        Console.WriteLine("Connection closed prematurely");
+                        break;
+                    }
+                    totalBytesRcvd += bytesRcvd;
+                }
+
+                Console.WriteLine("Received {0} bytes from server: {1}", totalBytesRcvd, Encoding.ASCII.GetString(byteBuffer, 0, totalBytesRcvd));
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } finally
+            {
+                sock.Close();
+            }
+        }
+    }
+}
---
+++ Chapter 2/TcpEchoServerSocket/TcpEchoServerSocket/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpEchoServerSocket
+{
+    class TcpEchoServerSocket
+    {
+        private const int BUFFSIZE = 32; // receive bufer size
+        private const int BACKLOG = 5; // outstanding connection queue max size
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                throw new ArgumentException("Parameters: [<Port>]");
+            }
+
+            int servPort = (args.Length == 1) ? int.Parse(args[0]) : 7;
+
+            Socket server = null;
+
+            try
+            {
+                // Create a socket to accept client connections
+                server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Associate the socket with a local address and port number
+                server.Bind(new IPEndPoint(IPAddress.Any, servPort));
+                server.Listen(BACKLOG);
+            }
+            catch (SocketException e) 
+            {
+                Console.WriteLine(e.ErrorCode + ": " + e.Message);
+                Environment.Exit(e.ErrorCode);
+            }
+
+            byte[] rcvBuffr = new byte[BUFFSIZE];
+            int bytesRcvd;
+
+            for (; ;)
+            {
+                Socket client = null;
+
+                try
+                {
+                    client = server.Accept(); // get client connection
+                    Console.WriteLine("Handling client at " + client.RemoteEndPoint + " - ");
+
+                    int totalBytesEchoed = 0;
+                    while ((bytesRcvd = client.Receive(rcvBuffr,0, rcvBuffr.Length, SocketFlags.None)) > 0)
+                    {
+                        client.Send(rcvBuffr, 0, bytesRcvd, SocketFlags.None);
+                        totalBytesEchoed += bytesRcvd;
+                    }
+                    Console.WriteLine("echoed {0} bytes", totalBytesEchoed);
+                    client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    client.Close();
+                }
+            }
+         
+        }
+    }
+}
+
